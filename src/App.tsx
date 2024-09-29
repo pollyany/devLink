4c4b905d1a596b0ce3b2943d2ec88a4640f9b575
kerley-pollyany <kerleypollyany@gmail.com>
2024-09-29 14:59:13 -0300

    Protegendo rota admin

--- src/App.tsx
+++ src/App.tsx
@@ -3,25 +3,33 @@ import Home from "./pages/home";
 import Admin from "./pages/admin";
 import Networks from "./pages/networks";
 import Login from "./pages/login";
+import Private from "./routes/Private";
 
 const router = createBrowserRouter([
   {
-    path: '/',
-    element: <Home/>
+    path: "/",
+    element: <Home />,
   },
   {
-    path: '/login',
-    element: <Login/>
+    path: "/login",
+    element: <Login />,
   },
   {
-    path: '/admin',
-    element: <Admin/>
+    path: "/admin",
+    element: (
+      <Private>
+        <Admin />
+      </Private>
+    ),
   },
   {
-    path: '/admin/social',
-    element: <Networks/>
+    path: "/admin/social",
+    element: (
+      <Private>
+        <Networks />
+      </Private>
+    ),
   },
-])
+]);
 
-
-export {router};
+export { router };
